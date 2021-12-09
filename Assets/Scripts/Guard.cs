e5605ae323ed163cd6e9bdbc5c77dfdad91fc08d
Nicholas Chai <nicholas.chai@outlook.com>
2021-12-09 21:02:23 +0800

    added in the fixed update to move the guard to it's various waypoints adjusted the draw gizmos function to just draw the waypoints and thier coresponding lines instead of the guard start position snapped the guard to the first waypoint position at startup

--- Assets/Scripts/Guard.cs
+++ Assets/Scripts/Guard.cs
@@ -5,19 +5,70 @@ using UnityEngine;
 public class Guard : MonoBehaviour
 {
     public Transform pathholder;
+    public float speed;
+    public float waitTime;
+    float timer;
+    Transform currentWaypoint;
+    int waypointIndex;
+    
+    void Start()
+    {
+        waypointIndex = 0;
+        currentWaypoint = pathholder.GetChild(waypointIndex);
+        speed = 5f;
+        waitTime = 2f;
+        timer = 0f;
+
+        // snap the guard to his starting position
+        transform.position = currentWaypoint.position;
+    } 
+
+    void Update()
+    {
+
+
+    }
+
+    private void FixedUpdate()
+    {
+        // if we've reached the target waypoint then stop if now keep moving toward it
+        if (Vector3.Distance(transform.position, currentWaypoint.position) < 0.2f)
+        {
+            // if the timer is finished then set new waypoint and reset timer
+            if (timer <= 0)
+            {
+                // change the current waypoint to the next one but if the index is larger than the possible
+                // number of waypoints then set the waypoint back to the first waypoint again
+                waypointIndex += 1;
+                if (waypointIndex >= pathholder.childCount)
+                {
+                    waypointIndex = 0;
+                }
+                currentWaypoint = pathholder.GetChild(waypointIndex);
+                timer = waitTime;
+            } else // keep the timer ticking along while the guard has stopped moving
+            {
+                timer -= Time.fixedDeltaTime;
+            }
+        } else // move toward the current waypoint
+        {
+            Vector3 direction = (currentWaypoint.position - transform.position).normalized;
+            transform.position += direction * speed * Time.fixedDeltaTime;
+        }
+    }
 
     private void OnDrawGizmos()
     {
-        Vector3 startPosition = transform.position;
+        Vector3 startPosition = pathholder.GetChild(0).position;
         Vector3 previousPosition = startPosition;
 
         // loop through all the waypoints in the given pathholder and draw gizmos spheres for each
         for (int i = 0; i < pathholder.childCount; i++)
         {
-            Transform currentWaypoint = pathholder.GetChild(i);
-            Gizmos.DrawSphere(currentWaypoint.position, 0.3f);
-            Gizmos.DrawLine(previousPosition, currentWaypoint.position);
-            previousPosition = currentWaypoint.position;
+            Transform currentGizmoWaypoint = pathholder.GetChild(i);
+            Gizmos.DrawSphere(currentGizmoWaypoint.position, 0.3f);
+            Gizmos.DrawLine(previousPosition, currentGizmoWaypoint.position);
+            previousPosition = currentGizmoWaypoint.position;
         }
 
         Gizmos.DrawLine(previousPosition, startPosition);
